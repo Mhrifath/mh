964f8f7a0fd884f667782f15b6e900445e89d596
Mhrifath <33103356+Mhrifath@users.noreply.github.com>
2018-09-19 21:52:49 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$(function(){
+    
+   new TypeIt('.type', {
+       strings: ['My name is Mohammad Rifath', 'I am a Web designer'],
+       loop: true,
+       breakLines: false,
+       deleteSpeed: 100,
+   });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
